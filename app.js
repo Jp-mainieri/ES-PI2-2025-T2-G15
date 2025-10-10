1852e6e2c50cad89ee57260167dd2e707192deff
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-10-10 12:39:23 -0300

    criei o `app.js` e criei a rota default do express.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser  = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+const port = 3000; //Define o número da porta
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//ROTAS
+
+app.get('/', (req, res) => {
+    res.send('Tudo Funcionando');
+});
+
+app.listen(port, () => {});
+
+
+
