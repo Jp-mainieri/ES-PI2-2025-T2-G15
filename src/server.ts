0bfc1a5955c6bd6eef416675a916b7b719a04f56
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-11-06 15:20:04 -0300

    deleted rotas estudantes on `server.ts`

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,6 @@ import express, {Request, Response} from "express";
 import bodyParser from "body-parser"
 
 // Imports de funções do CRUD
-import {
-    getAllEstudantes,
-    getEstudanteById,
-    addEstudante
-} from "./db/estudantes";
 
 import {
     getAllInstituicoes,
@@ -70,64 +65,6 @@ const port = 3000;
 app.use(bodyParser.json());
 
 // ROTAS:
-
-// Rotas de Estudantes:
-
-// Rota para obter todos os estudantes
-app.get('/estudantes', async (req :Request,res:Response)=>{
-    try {
-        const estudantes = await getAllEstudantes();
-        res.json(estudantes);
-    }catch (err){
-        console.error(err);
-        res.status(500).json({
-            error: "Erro ao buscar estudantes"
-        })
-    }
-})
-
-// Rota para obter um estudante por id
-app.get('/estudantes/:id', async(req:Request,res:Response)=>{
-    try{
-
-    const id = Number(req.params.id);
-    const estudante = await getEstudanteById(id);
-    if (estudante) {
-        res.json(estudante);
-    }else {
-        res.status(404).json({
-            message: "Estudante nao foi encontrado com o id fornecido."
-        })
-    }
-    }catch (err) {
-        console.error(err)
-        res.status(500).json({
-            error: "Erro ao buscar estudante pelo ID fornecido."
-        })
-    }
-})
-
-// Rota para inserir um estudante
-app.post('/estudantes', async (req:Request,res:Response)=>{
-    try {
-        const {ra,nome,email} = req.body;
-        if (!ra || !nome || !email){
-            return res.status(400).json({
-                error: "Campos RA, Nome e Email são Obrigatórios."
-            })
-        }
-        const id = await addEstudante(ra,nome,email);
-        res.status(200).json({
-            message: "Estudante Adicionado com sucesso.", id
-        })
-
-    }catch (err) {
-        console.error(err)
-        res.status(500).json({
-            error: "Erro ao inserir estudante."
-        });
-    }
-})
 
 // Rotas de Instituições:
 
